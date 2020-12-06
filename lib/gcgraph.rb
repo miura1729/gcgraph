a640ce1a7b0f81d618c85d7638de8c4c0c9b1ff8
Miura Hideki <m-72@tf6.so-net.ne.jp>
2020-12-06 19:39:20 +0900

    More generic

--- lib/gcgraph.rb
+++ lib/gcgraph.rb
@@ -13,7 +13,7 @@ module GCGraph
       @xscale = sc
       set_data_range(@ominx, @maxx, @miny, @maxy)
     end
-    
+
     def set_data_range(minx, maxx, miny, maxy)
       @ominx = minx.to_f
       @maxx = maxx.to_f
@@ -26,7 +26,7 @@ module GCGraph
         @minx = @ominx
       end
     end
-    
+
     def position_in_graph(x, y)
       if x < @minx or @maxx < x then
         return [nil, nil]
@@ -37,18 +37,18 @@ module GCGraph
 
       dx = @maxx - @minx
       posx = ((x - @minx) / dx) * @xsize
-      
+
       dy = @maxy - @miny
       posy = ((y - @miny) / dy) * @ysize
-      
+
       [posx, posy]
     end
   end
-  
+
   class GraphGenCanvas<GraphGen
     NUM_VLABEL = 10
     NUM_HLABEL = 5
-    
+
     BODRER_SCRIPT_TEMPLATE = <<EOS
     function draw_border() {
       var canctx = document.getElementById('border').getContext('2d')
@@ -96,40 +96,17 @@ EOS
     end
 
     def set_data(data)
-      xmin = nil
-      xmax = nil
-      ymin = nil
-      ymax = nil
-      @data = []
-      data.each do |gd|
-        sd = gd.sort_by {|a| a[0]}
-        sdx = sd.map {|a| a[0]}
-        sdy = sd.map {|a| a[1]}
-        cymin = sdy.min
-        if ymin == nil or cymin < ymin then
-          ymin = cymin
-        end
-        if ymin > 0 then
-          ymin = 0
-        end
-
-        cymax = 10 ** ((Math.log10(sdy.max + 1.1)).to_i + 1)
-        if ymax == nil or ymax < cymax then
-          ymax = cymax
-        end
-
-        cxmin = sdx.min
-        if xmin == nil or cxmin < xmin then
-          xmin = cxmin
-        end
-
-        cxmax = sdx.max
-        if xmax == nil or xmax < cxmax then
-          xmax = cxmax
-        end
-
-        @data.push sd
+      if data[0].size > 100 then
+        data2 = [data[0][-100, 100], data[1][-100, 100], data[2][-100, 100]]
+      else
+        data2 = data
       end
+      xmin = data2[0].min
+      xmax = data2[0].max
+      ymin = 0
+      ymax = data2[2].max
+      p data2
+      @data = data2
 
       set_data_range(xmin, xmax, ymin, ymax)
     end
@@ -140,23 +117,28 @@ EOS
 
     GraphColor = ['Red', 'Blue']
     def graph_script
-      res = "canctx.clearRect(0, 0, #{@xsize}, #{@ysize});\n" 
-      @data.each_with_index do |gd, i|
-        agraph = ""
-        gd.each do |a|
-          x, y = position_in_graph(a[0], a[1])
+      res = "canctx.clearRect(0, 0, #{@xsize}, #{@ysize});\n"
+      [0, 1].each do |gno|
+        agraph = graph_script_set_line_prop(GraphColor[gno])
+        agraph += "canctx.beginPath();\n"
+        fstp = true
+        @data[0].each_with_index do |origx, i|
+          origy = @data[gno + 1][i]
+          x, y = position_in_graph(origx, origy)
           if x then
             y = @ysize - y
-            agraph += "canctx.moveTo(#{x}, #{@ysize});\n"
+            if fstp then
+              agraph += "canctx.moveTo(#{x}, #{y});\n"
+              fstp = false
+            end
             agraph += "canctx.lineTo(#{x}, #{y});\n"
           end
+          res += agraph
+          res += "canctx.stroke();\n"
         end
-
-        res += graph_script_set_line_prop(GraphColor[i])
-        res += "canctx.beginPath();\n"
-        res += agraph
-        res += "canctx.stroke();"
+        res
       end
+
       graph_script_begin + res
     end
 
@@ -168,7 +150,7 @@ EOS
         value = "left: 0px; top: #{iy  -  dy * i}px"
         res += "span#vlabel#{i} { #{value} }\n"
       end
-      
+
       res
     end
 
@@ -177,9 +159,9 @@ EOS
       iy = @miny
       dy = (@maxy - @miny) / NUM_VLABEL
       (0..NUM_VLABEL).each do |i|
-        res += "<span id=\"vlabel#{i}\"> #{iy  +  dy * i} </span>\n"
+        res += "<span id=\"vlabel#{i}\"> #{"%.2f" % [(iy  +  dy * i)]} </span>\n"
       end
-      
+
       res
     end
 
@@ -189,9 +171,9 @@ EOS
       dy = (@maxy - @miny) / NUM_VLABEL
       (0..NUM_VLABEL).each do |i|
         dest = "document.getElementById(\"vlabel#{i}\").innerHTML"
-        res += "#{dest} = #{iy  +  dy * i};\n"
+        res += "#{dest} = #{"%.2f" % [iy  +  dy * i]};\n"
       end
-      
+
       res
     end
 
@@ -203,7 +185,7 @@ EOS
         value = "left: #{dx * i + ix}px; top: #{@ysize + 50}px"
         res += "span#hlabel#{i} { #{value} }\n"
       end
-      
+
       res
     end
 
@@ -211,9 +193,9 @@ EOS
       res = ""
       dx = (@maxx - @minx) / NUM_HLABEL
       (0..NUM_HLABEL).each do |i|
-        res += "<span id=\"hlabel#{i}\"> #{dx * i + @minx} </span>\n"
+        res += "<span id=\"hlabel#{i}\"> #{"%.2f" % [(dx * i + @minx)]} </span>\n"
       end
-      
+
       res
     end
 
@@ -222,10 +204,14 @@ EOS
       dx = (@maxx - @minx) / NUM_HLABEL
       (0..NUM_HLABEL).each do |i|
         dest = "document.getElementById(\"hlabel#{i}\").innerHTML"
-        res += "#{dest} =  #{dx * i + @minx};\n"
+        res += "#{dest} =  #{"%.2f" % [dx * i + @minx]};\n"
       end
-      
+
       res
+    end
+
+    def graph_css
+      "left: #{@maxy.to_s.size * 12}px; top: 50px"
     end
   end
 
@@ -237,14 +223,14 @@ EOS
   <style type="text/css">
     canvas {position: absolute}
     span {position: absolute}
-    canvas#border { left: 40px; top: 50px }
-    canvas#graph { left: 40px; top: 50px }
+    canvas#border { <%= @graph_gen.graph_css %> }
+    canvas#graph { <%= @graph_gen.graph_css %> }
     div#vlabel span {text-align: right; width: 30px }
     div#hlabel span {text-align: left; width: 30px }
     <%= @graph_gen.vlabel_css %>
     <%= @graph_gen.hlabel_css %>
   </style>
-    
+
   <script type="text/javascript" id="gscript">
    <%= @graph_gen.border_script %>
   </script>
@@ -290,8 +276,8 @@ EOS
         if !defined? @graph_gen then
           @graph_gen = GraphGenCanvas.new(600, 400)
         end
-        
-        @graph_gen.set_data([[[0, 0], [1, 1000]]])
+
+        @graph_gen.set_data(ObjectCounter.gdata)
         res.body = ERB.new(SCRIPT).result(binding)
         res['Content-Type'] = "text/html"
       end
@@ -348,7 +334,7 @@ EOS
       @log = DummyLog.new
 
       @graph_gen = GraphGenCanvas.new(600, 400)
-      @server = HTTPServer.new(:Port => 8088, 
+      @server = HTTPServer.new(:Port => 8088,
                                :BindAddress => "localhost",
                                :Logger => @log,
                                :AccessLog => [])
@@ -361,29 +347,32 @@ EOS
   end
 
   class ObjectCounter
+    @@result = []
+    @@result[0] = [] # time
+    @@result[1] = [] # used
+    @@result[2] = [] # total
     def self.gdata
-      duse = []
-      dtotal = []
-      rep = GC::Profiler.result
-      rep = rep.split(/\n/)
-      rep.shift
-      rep.shift
-      
-      if rep.size < 2 then
-        rep = ["0 0 0 0 0", "0, 0.1, 1, 1, 1"] + rep
-      end
-      rep.each do |es|
-        ea = es.split(/\s+/)
-        duse.push [ea[2].to_f, ea[3].to_f]
-        dtotal.push [ea[2].to_f, ea[4].to_f]
-      end
-      [dtotal, duse]
+      @@result
+    end
+
+    def self.sampling
+      stat = ObjectSpace.count_objects
+      @@result[0].push Process.times.utime
+      @@result[1].push stat[:T_OBJECT]
+      @@result[2].push stat[:TOTAL]
     end
   end
-end  # GCGraph
+end
 
 GC::Profiler.enable
 GC.start
 Thread.new {
   a = GCGraph::GraphServer.new
+}.run
+
+Thread.new {
+  while true
+    GCGraph::ObjectCounter.sampling
+    sleep(0.1)
+  end
 }.run
